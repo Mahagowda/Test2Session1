eb9d09bba8416aaa4848f300f0ee2598af543174
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-02-21 06:33:36 +0000

    p1

---
+++ p2original.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void input_line(float *x1, float *y1)
+{
+  printf("enter the x1:");
+  scanf("%f",x1);
+  printf("enter the y1:");
+  scanf("%f",y1);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  
+}
